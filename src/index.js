6bc6d09660d595d2384293b0776b216b62c630de
Kauan <mvkauan4@gmail.com>
2023-09-15 15:27:30 -0300

    pagamento

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './pages/home';
+import App from './pages/cadastro';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Pagamento from './pages/pagamento';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/pagamento' element={<Pagamento />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/pagamento/index.js
+++ src/pages/pagamento/index.js
@@ -1,7 +1,46 @@
+import Cabecalho from '../../components/cabecalho';
 import './index.scss';
 
 export default function pagamento() {
   return (
+    <main className='pagina-pagamento'>
+      <div className='Cabecalho'>
+      <Cabecalho/>
+        </div>
 
-  )
+        <div className='tudo'>
+          
+          <div className='esq'>
+            
+            <div className='textos'>
+              <h1>Estamos quase lá... </h1>
+              <h1>Preencha a forma de pagamento</h1>
+            </div>
+            </div>
+
+          <div className='esq2'>
+            <div className='tipos'>
+            <img src='/assets/images/cartao.png'/>
+                <select>
+                  <option>Tipo de Cartão</option>
+                  <option>Débito</option>
+                  <option>Crédito</option>
+                </select>
+                </div>
+
+                <div className='tipos2'>
+                <input type='text' placeholder='Nome idêntico ao do cartão'/>
+                </div>
+
+                <div className='tipos3'>
+                <input className='nr' type='text' placeholder='Numero do cartão'/>
+                <input className='MM' type='text' placeholder='MM/YY'/>
+                <input className='CVV' type='text' placeholder='CVV'/>
+
+                </div>
+          </div>
+      
+        </div>
+    </main>
+  );
 }
