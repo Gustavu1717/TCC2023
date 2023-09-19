eb21ca397f2526b97874c256cc91cb6da03ba2f3
Kauan <mvkauan4@gmail.com>
2023-09-19 14:41:12 -0300

    nvpag

--- src/pages/pagamento/index.js
+++ src/pages/pagamento/index.js
@@ -6,77 +6,60 @@ export default function pagamento() {
     <main className='pagina-pagamento'>
       <div className='Cabecalho'>
       <Cabecalho/>
-        </div>
+      </div>
 
-      <div className='tudo'>
-          
-        <div className='esquerda'>
-          <div className='esq'>
-            
-            <div className='textos'>
-              <h1>Estamos quase lá... </h1>
-              <h1>Preencha a forma de pagamento</h1>
-            </div>
-            </div>
-
-          <div className='esq2'>
-            <div className='tipos'>
-            <img src='/assets/images/cartao.png'/>
-                <select>
-                  <option>Tipo de Cartão</option>
-                  <option>Débito</option>
-                  <option>Crédito</option>
-                </select>
-                </div>
-
-                <div className='tipos2'>
-                <input type='text' placeholder='Nome idêntico ao do cartão'/>
-                </div>
-
-                <div className='tipos3'>
-                <input className='nr' type='text' placeholder='Numero do cartão'/>
-                <input className='MM' type='text' placeholder='MM/YY'/>
-                <input className='CVV' type='text' placeholder='CVV'/>
-
+        <div className='tudo'>
+            <div className='esquerda'>
+              <div className='esq'>
+                
+                <div className='textos'>
+                  <h1>Estamos quase lá... </h1>
+                  <h1>Preencha a forma de pagamento</h1>
                 </div>
             </div>
-        </div>
 
-        <div className='direita'>
+            <div className='esq2'>
+                  <div className='tipos'>
+                    <img src='/assets/images/cartao.png'/>
+                    <select>
+                      <option>Tipo de Cartão</option>
+                      <option>Débito</option>
+                      <option>Crédito</option>
+                    </select>
+                  </div>
 
-          <div className='dir'>
+                  <div className='tipos2'>
+                    <input type='text' placeholder='Nome idêntico ao do cartão'/>
+                  </div>
 
-            <div className='textosloc'>
-              <img src='/assets/images/loc.png'/>
-              <h1>Preencha o endereço de entrega</h1>
+                  <div className='tipos3'>
+                    <input className='nr' type='text' placeholder='Numero do cartão'/>
+                    <input className='MM' type='text' placeholder='MM/YY'/>
+                    <input className='CVV' type='text' placeholder='CVV'/>
+                  </div>
+
+                  <div className='butao'>
+                    <button>Finalizar Compra</button>
+                  </div>
+
+                  <div className='seguro'>
+                    <img src='/assets/images/escudo.png'/>
+                    <h6>Pagamento Seguro</h6>
+                  </div>
+
             </div>
+          </div>
 
-            <div className='inputs'>
+          <div className='direita'>
 
-              <input className='endere' type='text' placeholder='Endereço'/>
-
-              <div className='cs'>
-
-                <input className='cepi' type='text' placeholder='CEP'/>
-                <input type='text' placeholder='Nº Casa'/>
-
+            <div className='quadrado'>
+              <div className='resumo'>
+                <h2>Resumo do pedido</h2>
               </div>
-
-            <div className='complementoo'>
-              <input type='text' placeholder='Complemento'/>
             </div>
-
-            <div className='confi'>
-              <button>Confirmar</button>
-            </div>
-
-            </div>
-
-
 
           </div>
-        </div>
-      </div>
+        </div> 
     </main>
   );
 }
